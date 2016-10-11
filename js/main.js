02d3b26f9ed3527606cc84026aa7af2ff9f514aa
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-11 15:39:12 -0600

    feat(core) Add functions (-, *, /, and %) to JS; Verify with console
    Created subtraction function and verified with console
    Created multiplication function and verified with console
    Created division function and verified with console
    Created remainder function and verified with console

--- js/main.js
+++ js/main.js
@@ -7,6 +7,26 @@ function addition (x, y) {
   return x + y;
 }
 
+function subtraction (x, y) {
+  // console.log(x + y);
+  return x - y;
+}
+
+function multiplication (x, y) {
+  // console.log(x + y);
+  return x * y;
+}
+
+function division (x, y) {
+  // console.log(x + y);
+  return x / y;
+}
+
+function remainder (x, y) {
+  // console.log(x + y);
+  return x % y;
+}
+
 function calculate (x, y, cb) {
   console.log (cb(x, y) );
   return cb(x, y);
