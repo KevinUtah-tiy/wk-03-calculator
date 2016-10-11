f63c90fa061ba8a703c995a3ff03fd12f40ff7a6
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-11 15:30:50 -0600

    feat(config) Callback function addition create, verify
    In main.js, created addition function and callback function
    Verify that it worked on console

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-function addItems (a , b) {
-  return "a + b"
-};
+console.clear();
+
+// console.log('Kevin sitting in class');
+
+function addition (x, y) {
+  // console.log(x + y);
+  return x + y;
+}
+
+function calculate (x, y, cb) {
+  console.log (cb(x, y) );
+  return cb(x, y);
+}
