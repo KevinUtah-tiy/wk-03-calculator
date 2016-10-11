93a6429c88f46d3ea62b92b9290f512d424d28aa
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-11 15:54:27 -0600

    feat(core) Change name of JS file; Delete console.log in js
    Changed name of js file from main.js to script.js
    Deleted unnecessary console.log from js file

--- js/script.js
+++ js/script.js
@@ -1,33 +1,27 @@
 console.clear();
 
-// console.log('Kevin sitting in class');
 
 function addition (x, y) {
-  // console.log(x + y);
   return x + y;
 }
 
 function subtraction (x, y) {
-  // console.log(x + y);
   return x - y;
 }
 
 function multiplication (x, y) {
-  // console.log(x + y);
   return x * y;
 }
 
 function division (x, y) {
-  // console.log(x + y);
   return x / y;
 }
 
 function remainder (x, y) {
-  // console.log(x + y);
   return x % y;
 }
 
 function calculate (x, y, cb) {
-  console.log (cb(x, y) );
+  console.log (cb(x, y));
   return cb(x, y);
 }
