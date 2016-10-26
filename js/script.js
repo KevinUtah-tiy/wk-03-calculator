a3e818b8bfa632061f683a41530a1b9952be146e
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-25 19:59:44 -0600

    feat(core) Delete to make DRY code
    Deleted multiple lines of querySelector
    Deleted multiple lines of display
    Deleted multiple lines of console.log

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 
 console.clear();
 
-// link buttons to an input
-// window.prompt ('');
-
 var arr = []; //global
 
 function reduceToOutput (arr) {
@@ -31,28 +28,11 @@ function display (value, append) {
 
 function userInput (value) {
 
-
-
-
-  // document.querySelector('.display').innerHTML = arr;
-  // function reduceToOutput (arr) {
-  //   return arr.reduce(function(previousValue, currentValue) {
-  //     console.log(previousValue, currentValue);
-  //     previousValue = previousValue || "";
-  //     currentValue = previousValue + currentValue;
-  //     return currentValue;
-  //   });
-  // }
-
   arr.push(value);
   let output = reduceToOutput(arr)
   display(output, false);
 
-  console.log(arr);
-  // return arr;
-  console.log(arr[1]);
 }
-
 
 /* ----  DO NOTHING TO THESE FUNCTIONS ------- */
 function addition (x, y) {
@@ -76,59 +56,28 @@ function remainder (x, y) {
 }
 
 function calculate (x, cb, y) {
-  // console.log (cb(x, y));
   let result = cb(x, y);
   display(result, false);
 }
-
-
 
 function prepCalculate(arr) {
   let x = parseInt(arr[0]);
   let y = parseInt(arr[2]);
 
   if (arr[1] === "+") {
-    // display (calculate(x, addition, y));
     console.log(calculate (x, addition, y));
-    // document.querySelector('.display').innerHTML = calculate (x, addition, y);
   } else
   if (arr[1] === "-") {
-    // display(calculate (x, addition, y));
       calculate(x, subtraction, y);
-      // console.log(calculate (x, subtraction, y));
-      // document.querySelector('.display').innerHTML = calculate (x, subtraction, y);
     } else
   if (arr[1] === "*") {
     console.log(calculate (x, multiplication, y));
       calculate(x, multiplication, y);
-      // console.log(calculate (x, multiplication, y));
-      // document.querySelector('.display').innerHTML = calculate (x, multiplication, y);
     } else
     if (arr[1] === "/") {
-      // console.log(calculate (x, division, y));
       calculate (x, division, y);
-      // document.querySelector('.display').innerHTML = calculate (x, division, y);
     } else
     if (arr[1] === "%") {
-      // console.log(calculate(x, remainder, y));
       calculate (x, remainder, y);
-      // document.querySelector('.display').innerHTML = calculate (x, remainder, y);
     }
 }
-
-
-/*
-equalFunction() {
-("...") --->xyz
-ans = calculate (x, y, z)
-display(answ)
-}
-*/
-
-// function onPageLoad() {
-//   clearGuess();
-//
-// }
-
-// document.querySelector('button').onclick = onGuessCheck;
-// window.onLoad = onPageLoad;
